fa471242d047a594c3fbdbd5c44c20c99e6d1e18
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-23 15:00:01 +0900

    문제 05-1 [scanf 함수와 아스키 코드] 문제 1

--- Hello.C
+++ Hello.C
@@ -1,18 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 int main(void)
 {
-	char ch = 9;
-	int inum = 1052;
-	double dnum = 3.1415;
-	printf("size of variable ch: %d \n", sizeof(ch));
-	printf("size of variable inum: %d \n", sizeof(inum));
-	printf("size of variable dnum: %d \n", sizeof(dnum));
-
-	printf("size of char: %d \n", sizeof(char));
-	printf("size of int: %d \n", sizeof(int));
-	printf("size of long: %d \n", sizeof(long));
-	printf("size of long long: %d \n", sizeof(long long));
-	printf("size of float: %d \n", sizeof(float));
-	printf("size of double: %d \n", sizeof(double));
+	int leftUpX = 0, leftUpY = 0, rightDownX = 0, rightDownY = 0;
+	printf("좌 상단의 x, y 좌표: ");
+	scanf("%d %d", &leftUpX, &leftUpY);
+	printf("우 하단의 x, y 좌표: ");
+	scanf("%d %d", &rightDownX, &rightDownY);
+	printf("두 점이 이루는 직사각형의 넓이는 %d 입니다.", abs((rightDownX - leftUpX) * (leftUpY - rightDownY)));
 	return 0;
 }
