1590a3eb6eed9826febb1911447e3fb9f7b5880c
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-23 14:01:13 +0900

    SizeOfOperator.c

--- Hello.C
+++ Hello.C
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
-	int num=3, result;
-	result = num << 3;
-	result = result >> 2;
-	printf("result of this process: %d", result);
+	char ch = 9;
+	int inum = 1052;
+	double dnum = 3.1415;
+	printf("size of variable ch: %d \n", sizeof(ch));
+	printf("size of variable inum: %d \n", sizeof(inum));
+	printf("size of variable dnum: %d \n", sizeof(dnum));
+
+	printf("size of char: %d \n", sizeof(char));
+	printf("size of int: %d \n", sizeof(int));
+	printf("size of long: %d \n", sizeof(long));
+	printf("size of long long: %d \n", sizeof(long long));
+	printf("size of float: %d \n", sizeof(float));
+	printf("size of double: %d \n", sizeof(double));
 	return 0;
 }
