365593c89210ed2d6667d10aa26374e43291d35f
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-23 15:03:59 +0900

    문제 05-1 [scanf 함수와 아스키 코드] 문제 2

--- Hello.C
+++ Hello.C
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 int main(void)
 {
-	int leftUpX = 0, leftUpY = 0, rightDownX = 0, rightDownY = 0;
-	printf("좌 상단의 x, y 좌표: ");
-	scanf("%d %d", &leftUpX, &leftUpY);
-	printf("우 하단의 x, y 좌표: ");
-	scanf("%d %d", &rightDownX, &rightDownY);
-	printf("두 점이 이루는 직사각형의 넓이는 %d 입니다.", abs((rightDownX - leftUpX) * (leftUpY - rightDownY)));
+	double num1=0, num2=0;
+	printf("enter 2 numbers: ");
+	scanf("%lf %lf", &num1, &num2);
+	printf("%f + %f = %f\n", num1, num2, num1 + num2);
+	printf("%f - %f = %f\n", num1, num2, num1 - num2);
+	printf("%f * %f = %f\n", num1, num2, num1 * num2);
+	printf("%f / %f = %f\n", num1, num2, num1 / num2);
 	return 0;
 }
