ba7b5f062c3752bfa978794d636585c9e7470de7
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-23 15:08:00 +0900

    문제 05-1 [scanf 함수와 아스키 코드] 문제 4

--- Hello.C
+++ Hello.C
@@ -1,12 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-	double num1=0, num2=0;
-	printf("enter 2 numbers: ");
-	scanf("%lf %lf", &num1, &num2);
-	printf("%f + %f = %f\n", num1, num2, num1 + num2);
-	printf("%f - %f = %f\n", num1, num2, num1 - num2);
-	printf("%f * %f = %f\n", num1, num2, num1 * num2);
-	printf("%f / %f = %f\n", num1, num2, num1 / num2);
+	int num=0;
+	printf("enter 1 numbers: ");
+	scanf("%d", &num);
+	printf(" it's \"%c\" ! ", num);
 	return 0;
 }
