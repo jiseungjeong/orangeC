cdd124ede1df4e6ebaeb3f7f79893cc2e67b5367
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-31 17:04:03 +0900

    문제 08-1 [if~else문의 활용] 문제 3

--- Hello.C
+++ Hello.C
@@ -1,15 +1,29 @@
 #include <stdio.h>
 int main(void)
 {
-	int num1 = 0, num2 = 0;
-	scanf("%d %d", &num1, &num2);
-	if (num1 > num2)
+	int korScore = 0, engScore=0, mathScore = 0, average=0;
+	printf("input your korean score, english score, and math score: ");
+	scanf("%d %d %d", &korScore, &engScore, &mathScore);
+	average = ((double)korScore + (double)engScore + (double)mathScore) / 3;
+	if (average >= 90)
 	{
-		printf("%d - %d = %d\n", num1, num2, num1 - num2);
+		printf("Score: A");
+	}
+	else if (average >= 80)
+	{
+		printf("Score: B");
+	}
+	else if (average >=70)
+	{
+		printf("Score: C");
+	}
+	else if (average >= 50)
+	{
+		printf("Score: D");
 	}
 	else
 	{
-		printf("%d - %d = %d\n", num2, num1, num2 - num1);
+		printf("Score: F");
 	}
 	return 0;
 }
