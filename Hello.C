aead30730e97a7b6f3bb5bc27448de4b47f3e993
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-31 14:29:40 +0900

    문제 07-3 [while문과 do~while문에 익숙해지기] 문제1 방법 2

--- Hello.C
+++ Hello.C
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	int total = 0, num = 1;
+	int total = 0, num = 0;
+	printf("input integer number(0 to quit): ");
+	scanf("%d", &num);
+	total += num;
 	while (num != 0)
 	{
 		printf("input integer number(0 to quit): ");
