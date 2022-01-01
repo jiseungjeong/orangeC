367a9234bba0fd3b5bcd835e6a9d10c5a7479f1c
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-01 20:17:36 +0900

    도전! 프로그래밍1 도전 2

--- Hello.C
+++ Hello.C
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+void gugudan(int startDan, int endDan);
 
 int main(void)
 {
-	int num = 0;
-	scanf("%d", &num);
-	printf("%#x", num);
+	int num1 = 0, num2 = 0, bigNum = 0, smallNum=0;
+	printf("input 2 numbers: ");
+	scanf("%d %d", &num1, &num2);
+	bigNum = (num1 > num2) ? num1 : num2;
+	smallNum = (num1 < num2) ? num1 : num2;
+	gugudan(smallNum, bigNum);
 	return 0;
 }
 
+void gugudan(int startDan, int endDan)
+{
+	for (int i = startDan; i <= endDan; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			printf("%d X %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+	}
+}
