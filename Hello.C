9b7c4189698eaaedd9373f6ad5d998d4e03d87c6
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-01 21:39:30 +0900

    도전! 프로그래밍1 도전 4

--- Hello.C
+++ Hello.C
@@ -1,26 +1,41 @@
 #include <stdio.h>
-int GCD(int sNum, int bNum);
+
+int maxBuy(int moneyHave, int snack);
 
 int main(void)
 {
-	int num1 = 0, num2 = 0, bigNum=0, smallNum=0, gcd=0;
-	printf("input 2 numbers: ");
-	scanf("%d %d", &num1, &num2);
-	bigNum = (num1 > num2) ? num1 : num2;
-	smallNum = (num1 < num2) ? num1 : num2;
-	printf("GCD: %d", GCD(smallNum, bigNum));
-	return 0;
-}
-
-int GCD(int sNum, int bNum)
-{
-	int interNum = 0;
-	if (bNum % sNum == 0)
+	int money = 3500, interMoney1=0, interMoney2 = 0, interMoney3 = 0;
+	int shrimpSnack = 700, creamBread = 500, coke = 400; // shrimpSnack: 700won, creamBread: 500won, coke: 400won
+	int maxSS = 0, maxCB = 0, maxCoke = 0;
+	printf("the money that you have in present: 3500 won\n");
+	money = money - shrimpSnack - creamBread - coke;
+	maxSS = money / shrimpSnack;
+	maxCB = money / creamBread;
+	maxCoke = money / coke;
+	interMoney1 = money;
+	for (int i = 0; i <=maxSS; i++)
 	{
-		return sNum;
+		money = interMoney1;
+		money -= shrimpSnack * i;
+		interMoney2 = money;
+		for (int j = 0; j <= maxCB; j++)
+		{
+			money = interMoney2;
+			money -= creamBread * j;
+			if (money < 0)
+				continue;
+			interMoney3 = money;
+			for (int k = 0; k <= maxCoke; k++)
+			{
+				money = interMoney3;
+				money -= coke * k;
+				if (money == 0)
+				{
+					printf("creamBread: %d , shrimpSnack: %d , coke: %d \n", j+1, i+1, k+1);
+				}
+			}
+		}
 	}
-	interNum = bNum;
-	bNum = sNum;
-	sNum = interNum % sNum;
-	GCD(sNum, bNum);
+	printf("how do you buy?\n");
+	return 0;
 }
