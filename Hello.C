417267cf7967d0f274ecd264e0caf9fa03d1b4e5
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-01 20:09:10 +0900

    도전! 프로그래밍1 도전 1

--- Hello.C
+++ Hello.C
@@ -1,26 +1,11 @@
 #include <stdio.h>
 
-int nextPibo(int num1, int num2);
 
 int main(void)
 {
-	int num1 = 0, num2=1, cnt=0, interNum=0;
-	printf("how much piboNum do you print? : ");
-	scanf("%d", &cnt);
-	printf("%d, %d", num1, num2);
-	for (int i = 2; i < cnt; i++)
-	{
-		printf(", %d", nextPibo(num1, num2));
-		interNum = num2;
-		num2 = nextPibo(num1, num2);
-		num1 = interNum;
-	}
-
+	int num = 0;
+	scanf("%d", &num);
+	printf("%#x", num);
 	return 0;
-}
-
-int nextPibo(int num1, int num2)
-{
-	return num1 + num2;
 }
 
