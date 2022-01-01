de7fa3ec1fb87256fef940153f196a4c7f378594
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-01 16:29:46 +0900

    문제 09-1 [다양한 함수 정의하기] 문제 1

--- Hello.C
+++ Hello.C
@@ -1,23 +1,24 @@
 #include <stdio.h>
+int smallestValue(int num1, int num2, int num3);
 int main(void)
 {
-	int n = 0;
-	printf("input n: ");
-	scanf("%d", &n);
-	n /= 10;
-	switch (n)
-	{
-	case 0:
-		printf("0 or more and less than 10");
-		break;	
-	case 1:
-		printf("10 or more and less than 20");
-		break;
-	case 2:
-		printf("20 or more and less than 30");
-		break;
-	default:
-		printf("30 or more");
-	}
+	int input1 = 0, input2 = 0, input3 = 0;
+	printf("input 3 numbers: ");
+	scanf("%d %d %d", &input1, &input2, &input3);
+	printf("\nthe smallest value: ");
+	printf("%d\n", smallestValue(input1, input2, input3));
 	return 0;
+}
+int smallestValue(int num1, int num2, int num3)
+{
+	if (num1 < num2 && num1 < num3)
+	{
+		return num1;
+	}
+	else if (num2 < num3)
+	{
+		return num2;
+	}
+	else
+		return num3;
 }
