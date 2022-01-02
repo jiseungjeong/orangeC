e963a6ab8b43829d57451ed00e6904c5d6aa6c4c
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-02 20:22:35 +0900

    도전! 프로그래밍1 도전 7

--- Hello.C
+++ Hello.C
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
+int powerOfTwo(int num);
 
 int main(void)
 {
-	int sec = 0, min=0, hour=0;
-	printf("input seconds: ");
-	scanf("%d", &sec);
-	hour = sec / 3600;
-	sec %= 3600;
-	min = sec / 60;
-	sec %= 60;
-	printf("[h:%d, m:%d, s:%d]", hour, min, sec);
+	int n = 0, k;
+	printf("input constant n: ");
+	scanf("%d", &n);
+	for (k = 0; powerOfTwo(k) < n; k++)
+	{
+		continue;
+	}
+	printf("the maximum of k satisfying the equation: %d", k);
 	return 0;
+}
+
+int powerOfTwo(int num)
+{
+	int two = 1;
+	for (int i = 0; i < num; i++)
+	{
+		two *= 2;
+	}
+	return two;
 }
 
 
