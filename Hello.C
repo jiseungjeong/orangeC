911cab961e736dc7d9162b66b06e10d5e11870bf
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-02 20:07:21 +0900

    도전! 프로그래밍1 도전 5

--- Hello.C
+++ Hello.C
@@ -1,41 +1,33 @@
 #include <stdio.h>
 
-int maxBuy(int moneyHave, int snack);
+int primeDecision(int num);
 
 int main(void)
 {
-	int money = 3500, interMoney1=0, interMoney2 = 0, interMoney3 = 0;
-	int shrimpSnack = 700, creamBread = 500, coke = 400; // shrimpSnack: 700won, creamBread: 500won, coke: 400won
-	int maxSS = 0, maxCB = 0, maxCoke = 0;
-	printf("the money that you have in present: 3500 won\n");
-	money = money - shrimpSnack - creamBread - coke;
-	maxSS = money / shrimpSnack;
-	maxCB = money / creamBread;
-	maxCoke = money / coke;
-	interMoney1 = money;
-	for (int i = 0; i <=maxSS; i++)
+	int primeNum = 10, cnt=0, targetNum=2;
+	while (cnt < primeNum)
 	{
-		money = interMoney1;
-		money -= shrimpSnack * i;
-		interMoney2 = money;
-		for (int j = 0; j <= maxCB; j++)
+		if (primeDecision(targetNum) == 1)
 		{
-			money = interMoney2;
-			money -= creamBread * j;
-			if (money < 0)
-				continue;
-			interMoney3 = money;
-			for (int k = 0; k <= maxCoke; k++)
-			{
-				money = interMoney3;
-				money -= coke * k;
-				if (money == 0)
-				{
-					printf("creamBread: %d , shrimpSnack: %d , coke: %d \n", j+1, i+1, k+1);
-				}
-			}
+			cnt++;
+			printf("%d ", targetNum);
 		}
+		targetNum++;
 	}
-	printf("how do you buy?\n");
 	return 0;
 }
+
+int primeDecision(int num)
+{
+	int divisorCnt = 0;
+	for (int i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			return 0;
+		}
+	}
+	
+	return 1;
+}
+
