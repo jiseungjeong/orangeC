fd8a0bbba9e061c37a9160ea55633c8e9bf1d543
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-03 17:07:55 +0900

    문제 13-1 [포인터를 이용한 배열의 접근] 문제 3 수정

--- Hello.C
+++ Hello.C
@@ -3,15 +3,16 @@
 int main(void)
 {
 	int arr[5] = { 1,2,3,4,5 };
+	int total = 0;
 	int* ptr = &arr[4];
 	for (int i = 0; i < 5; i++)
 	{
-		*ptr += 2;
-		ptr--;
+		total+=*ptr--;
 	}
 	for (int i = 0; i < 5; i++)
 	{
 		printf("%d ", arr[i]);
 	}
+	printf("total: %d", total);
 	return 0;
 }
