a2f05e31653e1cc86c7638a536eacadc8d6b6d93
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-03 14:49:03 +0900

    문제 11-2 [배열기반의 문자열 표현] 문제 2

--- Hello.C
+++ Hello.C
@@ -2,13 +2,20 @@
 
 int main(void)
 {
-	char str[100];
-	int i = 0;
+	char str[100] = {0}, interStr = '\0';
+	int len = 0;
 	scanf("%s", str);
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
-		i++;
+		len++;
 	}
-	printf("\nword length: %d", i+1);
+	for (int j = 1; j < len / 2+1; j++)
+	{
+		interStr = str[len - 1];
+		str[j - 1] = str[len - j];
+		str[len - j] = interStr;
+
+	}
+	printf("reversed array: %s", str);
 	return 0;
 }
