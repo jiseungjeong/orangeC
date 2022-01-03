81b9c339ee915d610bc8180bd303e4b0a9377d31
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-03 12:55:20 +0900

    문제 11-1 [1차원 배열의 활용] 문제 2

--- Hello.C
+++ Hello.C
@@ -2,30 +2,11 @@
 
 int main(void)
 {
-	int arr[5], total=0, max=0, min=0;
-	for (int i = 0; i < 5; i++)
+	char arr[9] = { 'G','o','o','d',' ','t','i','m','e' };
+	for (int i = 0; i < 9; i++)
 	{
-		printf("input %d-th integer element: ", i);
-		scanf("%d", &arr[i]);
-		printf("\n");
+		printf("%c", arr[i]);
 	}
-
-	max = arr[0];
-	min = arr[0];
-	for (int i = 0; i < 5; i++)
-	{
-		total += arr[i];
-
-		if (max < arr[i])
-		{
-			max = arr[i];
-		}
-		if (min > arr[i])
-		{
-			min = arr[i];
-		}
-	}
-	printf("max: %d\nmin: %d\ntotal: %d\n", max, min, total);
 	return 0;
 }
 
