a005a3b6c75675738444e761036f5fdd0a1d3e09
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-04 08:16:23 +0900

    ArrayParam

--- Hello.C
+++ Hello.C
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+void ShowArayElem(int* param, int len)
+{
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		printf("%d ", param[i]);
+	}
+	printf("\n");
+}
+
 int main(void)
 {
-	char* strArr[3] = { "Simple", "String", "Array" };
-	printf("%s \n", strArr[0]);
-	printf("%s \n", strArr[1]);
-	printf("%s \n", strArr[2]);
+	int arr1[3] = { 1,2,3 };
+	int arr2[5] = { 4,5,6,7,8 };
+	ShowArayElem(arr1, sizeof(arr1) / sizeof(int));
+	ShowArayElem(arr2, sizeof(arr2) / sizeof(int));
 	return 0;
 }
