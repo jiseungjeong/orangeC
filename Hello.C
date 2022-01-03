d75bd7649f0bdddb97b493f921aa3e11888e497e
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-04 08:34:51 +0900

    CallByRefSwap

--- Hello.C
+++ Hello.C
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void Swap(int n1, int n2)
+void Swap(int *ptr1, int *ptr2)
 {
-	int temp = n1;
-	n1 = n2;
-	n2 = temp;
-	printf("n1 n2: %d %d \n", n1, n2);
+	int temp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = temp;
+	printf("n1 n2: %d %d \n", *ptr1, *ptr2);
 }
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 	int num2 = 20;
 	printf("num1 num2: %d %d \n", num1, num2);
 
-	Swap(num1, num2);
+	Swap(&num1, &num2);
 	printf("num1 num2: %d %d \n", num1, num2);
 	return 0;
 }
