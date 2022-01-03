4312799f718a0574531fbd7cc4ddb5f0743ee635
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-04 08:05:21 +0900

    StringArray

--- Hello.C
+++ Hello.C
@@ -2,12 +2,9 @@
 
 int main(void)
 {
-	int num1 = 10, num2 = 20, num3 = 30;
-	int* arr[3] = { &num1, &num2, &num3 };
-
-	printf("%d \n", *arr[0]);
-	printf("%d \n", *arr[1]);
-	printf("%d \n", *arr[2]);
-	
+	char* strArr[3] = { "Simple", "String", "Array" };
+	printf("%s \n", strArr[0]);
+	printf("%s \n", strArr[1]);
+	printf("%s \n", strArr[2]);
 	return 0;
 }
