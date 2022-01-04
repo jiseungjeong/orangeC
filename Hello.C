dc3ca97175d9624983a4e298e8d8b1ee0017d79c
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-04 17:19:40 +0900

    문제 16-1 [2차원 배열의 활용] 문제 3

--- Hello.C
+++ Hello.C
@@ -2,18 +2,28 @@
 
 int main(void)
 {
-	int arr[3][9];
-	for (int i = 0; i < 3; i++)
+	int arr[5][5] = {0};
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 0; j < 9; j++)
+		for (int j = 0; j < 4; j++)
 		{
-			arr[i][j] = (i + 1) * (j + 1);
+			printf("the score of %d-th subject of %d-th student: ", j, i);
+			scanf("%d", &arr[i][j]);
 		}
 	}
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 0; j < 9; j++)
-			printf("%-3d", arr[i][j]);
+		for (int j = 0; j < 4; j++)
+		{
+			arr[4][j] += arr[i][j];
+			arr[i][4] += arr[i][j];
+			arr[4][4] += arr[i][j];
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+			printf("%-3d ", arr[i][j]);
 		printf("\n");
 	}
 	return 0;
