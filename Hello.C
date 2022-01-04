d137ad473e002401bd552834eebbada648169818
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-04 15:56:13 +0900

    BubbleSort

--- Hello.C
+++ Hello.C
@@ -1,31 +1,32 @@
 #include <stdio.h>
-int strlen(char str[])
+
+void BubbleSort(int ary[], int len)
 {
-	int i = 0;
-	while (str[i] != 0 && i<100)
+	int i, j;
+	int temp;
+	for (i = 0; i < len - 1; i++)
 	{
-		i++;
-	}
-	return i;
-}
-void decisionPalindrome(char str[], int len)
-{
-	for (int i = 0; i < len/2; i++)
-	{
-		if (str[i] != str[len - i - 1])
+		for (j = 0; j < (len - i) - 1; j++)
 		{
-			printf("it's not palindrome.");
-			return;
+			if (ary[j] > ary[j + 1])
+			{
+				temp = ary[j];
+				ary[j] = ary[j + 1];
+				ary[j + 1] = temp;
+			}
 		}
 	}
-	printf("it's palindrome.");
 }
-
 int main(void)
 {
-	char word[100];
-	printf("input string: ");
-	scanf("%s", word);
-	decisionPalindrome(word, strlen(word));
+	int arr[4] = { 3,2,4,1 };
+	int i;
+
+	BubbleSort(arr, sizeof(arr) / sizeof(int));
+	for (i = 0; i < 4; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 	return 0;
 }
