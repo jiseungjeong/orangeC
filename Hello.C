14ef8f215fd82aedcb334c8005593f369dcb6b18
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-05 22:08:02 +0900

    2DArrParam

--- Hello.C
+++ Hello.C
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
+void ShowArr2DStyle(int(*arr)[4], int column)
+{
+	int i, j;
+	for (i = 0; i < column; i++)
+	{
+		for (j = 0; j < 4; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+}
+
+int Sum2DArr(int arr[][4], int column)
+{
+	int i, j, sum = 0;
+	for (i = 0; i < column; i++)
+		for (j = 0; j < 4; j++)
+			sum += arr[i][j];
+	return sum;
+}
+
 int main(void)
 {
-	int arr1[2][2] = {
-		{1,2},{3,4}
-	};
-	int arr2[3][2] = {
-		{1,2},{3,4},{5,6}
-	};
-	int arr3[4][2] = {
-		{1,2},{3,4},{5,6},{7,8}
-	};
+	int arr1[2][4] = { 1,2,3,4,5,6,7,8 };
+	int arr2[3][4] = { 1,1,1,1,3,3,3,3,5,5,5,5 };
 
-	int(*ptr)[2];
-	int i;
-	ptr = arr1;
-	printf("** Show 2,2 arr1 **\n");
-	for (i = 0; i < 2; i++)
-		printf("%d %d \n", ptr[i][0], ptr[i][1]);
-
-	ptr = arr2;
-	printf("** Show 3,2 arr2 **\n");
-	for (i = 0; i < 3; i++)
-		printf("%d %d \n", ptr[i][0], ptr[i][1]);
-	
-	ptr = arr3;
-	printf("** Show 4,2 arr3 **\n");
-	for (i = 0; i < 4; i++)
-		printf("%d %d \n", ptr[i][0], ptr[i][1]);
-
+	ShowArr2DStyle(arr1, sizeof(arr1) / sizeof(arr1[0]));
+	ShowArr2DStyle(arr2, sizeof(arr2) / sizeof(arr2[0]));
+	printf("arr1의 합: %d \n", Sum2DArr(arr1, sizeof(arr1) / sizeof(arr1[0])));
+	printf("arr2의 합: %d \n", Sum2DArr(arr2, sizeof(arr2) / sizeof(arr2[0])));
 	return 0;
 }
