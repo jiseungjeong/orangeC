c4b7e7a90968329d2c229d04425b78f3331effcb
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-05 15:45:03 +0900

    문제 17-1 [이중 포인터 변수의 활용]

--- Hello.C
+++ Hello.C
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+void MaxAndMin(int** dptr1, int** dptr2, int ary[])
+{
+	*dptr1 = &ary[0], *dptr2 = &ary[0];
+	for (int i = 0; i < 5; i++)
+	{
+		if (**dptr1 < ary[i])
+		{
+			*dptr1 = &ary[i];
+		}
+		if (**dptr2 > ary[i])
+		{
+			*dptr2 = &ary[i];
+		}
+	}
+}
+
 int main(void)
 {
-	int num = 100;
-	int* ptr = &num;
-	int** dptr = &ptr;
-	int*** tptr = &dptr;
-
-	printf("%d %d \n", **dptr, ***tptr);
+	int* maxPtr;
+	int* minPtr;
+	int arr[5] = { 1,2,3,5,4 };
+	MaxAndMin(&maxPtr, &minPtr, arr);
+	printf("max: %d \nmin: %d", *maxPtr, *minPtr);
 	return 0;
 }
