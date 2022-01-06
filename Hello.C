9ad420ad70a753173109df75e70926aa7a432b42
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-06 15:40:09 +0900

    VoidTypePointer

--- Hello.C
+++ Hello.C
@@ -1,43 +1,19 @@
 #include <stdio.h>
 
-int WhoIsFirst(int age1, int age2, int (*cmp)(int n1, int n2))
+void SoSimpleFunc(void)
 {
-	return cmp(age1, age2);
-}
-
-int OlderFirst(int age1, int age2)
-{
-	if (age1 > age2)
-		return age1;
-	else if (age1 < age2)
-		return age2;
-	else
-		return 0;
-}
-
-int YoungerFirst(int age1, int age2)
-{
-	if (age1 < age2)
-		return age1;
-	else if (age1 > age2)
-		return age2;
-	else
-		return 0;
+	printf("I'm so simple");
 }
 
 int main(void)
 {
-	int age1 = 20;
-	int age2 = 30;
-	int first;
+	int num = 20;
+	void* ptr;
 
-	printf("The order of entry 1 \n");
-	first = WhoIsFirst(age1, age2, OlderFirst);
-	printf("between %d age and %d, %d age first entrance!\n\n", age1, age2, first);
+	ptr = &num;
+	printf("%p \n", ptr);
 
-	printf("The order of entry 2 \n");
-	first = WhoIsFirst(age1, age2, YoungerFirst);
-	printf("between %d age and %d, %d age first entrance!\n\n", age1, age2, first);
-
+	ptr = SoSimpleFunc;
+	printf("%p \n", ptr);
 	return 0;
 }
