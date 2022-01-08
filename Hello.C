31fe7293ca6bb1e426b90d1b206a713cbbee04e5
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-08 21:20:49 +0900

    ConsoleEOF

--- Hello.C
+++ Hello.C
@@ -2,11 +2,14 @@
 
 int main(void)
 {
-	int ch1, ch2;
-	ch1 = getchar();
-	ch2 = fgetc(stdin);
+	int ch;
 
-	putchar(ch1);
-	fputc(ch2, stdout);
+	while (1)
+	{
+		ch = getchar();
+		if (ch == EOF)
+			break;
+		putchar(ch);
+	}
 	return 0;
 }
