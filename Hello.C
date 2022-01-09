30d1839e92fd213ae6ff82c60ff8763ec8fa983c
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-09 15:40:10 +0900

    문제 21-2 [문자열 처리] 문제 1

--- Hello.C
+++ Hello.C
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(void)
 {
-	char str[20];
-	printf("input integer: ");
+	char str[100];
+	int i = 0, total=0;
+	printf("input string: ");
 	scanf("%s", str);
-	printf("%d \n", atoi(str));
-
-	printf("input float number: ");
-	scanf("%s", str);
-	printf("%g \n", atof(str));
+	while (str[i] != '\0')
+	{
+		if((int)(str[i])>=48&&(int)(str[i])<=57)
+			total=total+(int)(str[i])-48;
+		i++;
+	}
+	printf("total: %d", total);
 	return 0;
 }
