b3944f3fe66de7aa32eed6574f97405b185d0a2b
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-10 17:11:39 +0900

    문제 22-1 [구조체의 정의]

--- Hello.C
+++ Hello.C
@@ -1,30 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-struct person
+struct employee
 {
 	char name[20];
-	char phoneNum[20];
-	int age;
+	char id[20];
+	int salaryInfo;
 };
 
 int main(void)
 {
-	struct person man1, man2;
-	strcpy(man1.name, "AhnSeongJun");
-	strcpy(man1.phoneNum, "010-1122-3344");
-	man1.age = 23;
+	struct employee employee1;
+	printf("input employee name: "); scanf("%s", employee1.name);
+	printf("input employee id: "); scanf("%s", employee1.id);
+	printf("input employee salaryInfo: "); scanf("%d", &(employee1.salaryInfo));
 
-	printf("input name: "); scanf("%s", man2.name);
-	printf("input phoneNumber: "); scanf("%s", man2.phoneNum);
-	printf("input age: "); scanf("%d", &(man2.age));
-
-	printf("name: %s \n", man1.name);
-	printf("phoneNum: %s \n", man1.phoneNum);
-	printf("age: %d \n", man1.age);
-	
-	printf("name: %s \n", man2.name);
-	printf("phoneNum: %s \n", man2.phoneNum);
-	printf("age: %d \n", man2.age);
+	printf("name: %s\n", employee1.name);
+	printf("id: %s\n", employee1.id);
+	printf("salaryInfo: %d\n", employee1.salaryInfo);
 	return 0;
 }
