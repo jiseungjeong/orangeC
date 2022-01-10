788b3472fa0160db22bf3f3a44300e124329d62d
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-10 17:28:24 +0900

    InitStructArray

--- Hello.C
+++ Hello.C
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-struct point
+struct person
 {
-	int xpos;
-	int ypos;
+	char name[20];
+	char phoneNum[20];
+	int age;
 };
 
 int main(void)
 {
-	struct point arr[3];
+	struct person arr[3] = {
+		{"leeSeungGi", "010-1212-0001",21},
+		{"jeongJiYoung", "010-1313-0002", 22},
+		{"hanJiSu","010-1717-0003", 19}
+	};
+
 	int i;
-
 	for (i = 0; i < 3; i++)
-	{
-		printf("input the coordinate of the point: ");
-		scanf("%d %d", &arr[i].xpos, &arr[i].ypos);
-	}
-
-	for (i = 0; i < 3; i++)
-		printf("[%d, %d] ", arr[i].xpos, arr[i].ypos);
+		printf("%-12s %s %d \n", arr[i].name, arr[i].phoneNum, arr[i].age);
 	return 0;
 }
