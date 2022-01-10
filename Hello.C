7b5b89eb9d42a7befcab2b5e14a668f31ac331d1
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-11 00:28:52 +0900

    FirstFileWrite

--- Hello.C
+++ Hello.C
@@ -1,37 +1,17 @@
 #include <stdio.h>
 
-typedef enum syllable
-{
-	Do=1, Re=2, Mi=3, Fa=4, So=5, La=6, Ti=7
-} Syllable;
-
-void Sound(Syllable sy)
-{
-	switch (sy)
-	{
-	case Do:
-		puts("Do is white Doraji."); return;
-	case Re:
-		puts("Re is round record."); return;
-	case Mi:
-		puts("Mi is blue Minari."); return;
-	case Fa:
-		puts("Fa is pretty FarangBird."); return;
-	case So:
-		puts("So is small solBangUl."); return;
-	case La:
-		puts("La is Ladio."); return;
-	case Ti:
-		puts("Ti is zolzol TiNatMul."); return;
-	}
-	puts("Let's sing together~ Do Re Mi Fa Sol La Ti Do Sol Do~ Bomb!!~");
-
-}
-
 int main(void)
 {
-	Syllable tone;
-	for (tone = Do; tone <= Ti; tone += 1)
-		Sound(tone);
+	FILE* fp = fopen("data.txt", "wt");
+	if (fp == NULL)
+	{
+		puts("file open failed!");
+		return -1;
+	}
+
+	fputc('A', fp);
+	fputc('B', fp);
+	fputc('C', fp);
+	fclose(fp);
 	return 0;
 }
