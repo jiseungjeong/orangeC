af5b96548d27254743af61c2e0459ede9f9a20a5
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-10 19:46:32 +0900

    StructTypedef

--- Hello.C
+++ Hello.C
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-typedef int INT;
-typedef int* PTR_INT;
+struct point
+{
+	int xpos;
+	int ypos;
+};
 
-typedef unsigned int UINT;
-typedef unsigned int* PTR_UINT;
+typedef struct point Point;
 
-typedef unsigned char UCHAR;
-typedef unsigned char* PTR_UCHAR;
+typedef struct person
+{
+	char name[20];
+	char phoneNum[20];
+	int age;
+} Person;
 
 int main(void)
 {
-	INT num1 = 120;
-	PTR_INT pnum1 = &num1;
-
-	UINT num2 = 190;
-	PTR_UINT pnum2 = &num2;
-
-	UCHAR ch = 'Z';
-	PTR_UCHAR pch = &ch;
-
-	printf("%d, %u, %c \n", *pnum1, *pnum2, *pch);
+	Point pos = { 10, 20 };
+	Person man = { "leeSeungGi", "010-1212-0001", 21 };
+	printf("%d %d \n", pos.xpos, pos.ypos);
+	printf("%s %s %d \n", man.name, man.phoneNum, man.age);
 	return 0;
 }
