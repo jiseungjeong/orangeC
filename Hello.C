08929bc7830f68a442b196de5170732536cc4aaa
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-10 17:05:18 +0900

    TelephoneInfo

--- Hello.C
+++ Hello.C
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
-struct point
+struct person
 {
-	int xpos;
-	int ypos;
+	char name[20];
+	char phoneNum[20];
+	int age;
 };
 
 int main(void)
 {
-	struct point pos1, pos2;
-	double distance;
+	struct person man1, man2;
+	strcpy(man1.name, "AhnSeongJun");
+	strcpy(man1.phoneNum, "010-1122-3344");
+	man1.age = 23;
 
-	fputs("point1 pos: ", stdout);
-	scanf("%d %d", &pos1.xpos, &pos1.ypos);
+	printf("input name: "); scanf("%s", man2.name);
+	printf("input phoneNumber: "); scanf("%s", man2.phoneNum);
+	printf("input age: "); scanf("%d", &(man2.age));
 
-	fputs("point2 pos: ", stdout);
-	scanf("%d %d", &pos2.xpos, &pos2.ypos);
-
-	distance = sqrt((double)((pos1.xpos - pos2.xpos) * (pos1.xpos - pos2.xpos) + (pos1.ypos - pos2.ypos) * (pos1.ypos - pos2.ypos)));
-
-	printf("distance between 2 points is %g", distance);
+	printf("name: %s \n", man1.name);
+	printf("phoneNum: %s \n", man1.phoneNum);
+	printf("age: %d \n", man1.age);
+	
+	printf("name: %s \n", man2.name);
+	printf("phoneNum: %s \n", man2.phoneNum);
+	printf("age: %d \n", man2.age);
 	return 0;
 }
