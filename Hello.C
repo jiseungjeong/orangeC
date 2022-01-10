292f2c657401d3d68ec3d5e2de42b6cdaaf6a9ad
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-10 18:26:14 +0900

    TypeNameTypeDef

--- Hello.C
+++ Hello.C
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 
-struct point
-{
-	int xpos;
-	int ypos;
-	struct point* ptr;
-};
+typedef int INT;
+typedef int* PTR_INT;
 
-struct person
-{
-	char name[20];
-	char phoneNum[20];
-	int age;
-};
+typedef unsigned int UINT;
+typedef unsigned int* PTR_UINT;
+
+typedef unsigned char UCHAR;
+typedef unsigned char* PTR_UCHAR;
 
 int main(void)
 {
-	struct point pos = { 10, 20 };
-	struct person man = { "leeSeungGi", "010-1212-0001",21 };
+	INT num1 = 120;
+	PTR_INT pnum1 = &num1;
 
-	printf("%p %p \n", &pos, &pos.xpos);
-	printf("%p %p \n", &man, man.name);
+	UINT num2 = 190;
+	PTR_UINT pnum2 = &num2;
+
+	UCHAR ch = 'Z';
+	PTR_UCHAR pch = &ch;
+
+	printf("%d, %u, %c \n", *pnum1, *pnum2, *pch);
 	return 0;
 }
