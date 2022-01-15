0ee094683c02d1b3f49a565f56aba8cbde441760
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-15 18:11:35 +0900

    문제 24-1 [문자열 방시그이 데이터 입출력] 문제 2

--- Hello.C
+++ Hello.C
@@ -2,15 +2,14 @@
 
 int main(void)
 {
-	FILE* fp = fopen("mystery.txt", "wt");
+	FILE* fp = fopen("mystery.txt", "at");
 	if (fp == NULL)
 	{
 		puts("file open failed!");
 		return -1;
 	}
-	fputs("#name: yoonSeongWoo\n", fp);
-	fputs("#id: 900208-1012589\n", fp);
-	fputs("#phoneNum: 010-1111-2222\n", fp);
+	fputs("#favorite food: jjamBbong, tangSuYuk\n", fp);
+	fputs("#hobby: soccer\n", fp);
 	fclose(fp);
 	return 0;
 }
