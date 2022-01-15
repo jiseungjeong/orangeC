7c58e775255ef5f281086c9615aefbfabd4d6080
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-15 18:24:06 +0900

    문제 24-1 [문자열 방식의 데이터 입출력] 문제 3

--- Hello.C
+++ Hello.C
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
-	FILE* fp = fopen("mystery.txt", "at");
+	FILE* fp = fopen("mystery.txt", "rt");
+	char str[20];
 	if (fp == NULL)
 	{
 		puts("file open failed!");
 		return -1;
 	}
-	fputs("#favorite food: jjamBbong, tangSuYuk\n", fp);
-	fputs("#hobby: soccer\n", fp);
+	while (fgets(str, sizeof(str), fp) != NULL) {
+		printf("%s", str);
+	}
+
 	fclose(fp);
 	return 0;
 }
