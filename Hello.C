40e982785421de3b7e9e0be7b65602a148e5dbd6
jiseungjeong <wjdwltmd1151@gmail.com>
2022-01-22 18:50:34 +0900

    DynamicMemoryAllocation

--- Hello.C
+++ Hello.C
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
-char name[30];
-
-char* ReadUserName(void)
-{
-	printf("What's your name? ");
-	gets(name);
-	return name;
-}
-
 int main(void)
 {
-	char* name1;
-	char* name2;
-	name1 = ReadUserName();
-	printf("name1: %s \n", name1);
-	name2 = ReadUserName();
-	printf("name2: %s \n", name2);
-	printf("name1: %s \n", name1);
-	printf("name2: %s \n", name2);
+	int* ptr1 = (int*)malloc(sizeof(int));
+	int* ptr2 = (int*)malloc(sizeof(int) * 7);
+	int i;
+
+	*ptr1 = 20;
+	for (i = 0; i < 7; i++)
+		ptr2[i] = i + 1;
+
+	printf("%d \n", *ptr1);
+	for (i = 0; i < 7; i++)
+		printf("%d ", ptr2[i]);
+
+	free(ptr1);
+	free(ptr2);
 	return 0;
 }
